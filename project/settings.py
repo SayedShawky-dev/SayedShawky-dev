0a7ea28441b835ac37d0b01811fb0d3847f7c13b
sayed shawky <sshawky@sure.com.sa>
2021-07-03 16:41:09 +0200

    add static & media config

--- project/settings.py
+++ project/settings.py
@@ -125,8 +125,14 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
+STATIC_ROOT = os.path.join(BASE_DIR,'project/static')
+
+MEDIA_URL='/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
