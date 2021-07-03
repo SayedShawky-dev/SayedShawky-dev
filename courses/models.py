4ffd41b862cfead4dca8a967b86fbedec5746249
sayed shawky <sshawky@sure.com.sa>
2021-07-03 13:32:26 +0200

    modify models

--- courses/models.py
+++ courses/models.py
@@ -16,15 +16,25 @@ class Course(models.Model):
     articles = models.ForeignKey('Article', on_delete=models.CASCADE)
     exercise = models.ForeignKey('Exercise', on_delete=models.CASCADE)
     
+    def __str__(self):
+        return self.name
+    
     
 class Article(models.Model):
     name = models.CharField(max_length=100)
     content = models.TextField(max_length=10000)
-
+    
+    def __str__(self):
+        return self.name
+        
 
 class Exercise(models.Model):
     name = models.CharField(max_length=100)
     question = models.CharField(max_length=500)
     answer = models.CharField(max_length=500)
+    
+    def __str__(self):
+        return self.name
+        
 
     
